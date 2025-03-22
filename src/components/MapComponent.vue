83922fff1d839abbafd20ba282dbf131c2b3a4c7
Marek Dworecki <marek.dworecki01@gmail.com>
2025-03-22 10:41:33 +0100

    added layer button

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,10 +1,14 @@
 <template>
     <div id="cesiumContainer"></div>
+    <div id="layerPicker">
+        <img src="../assets/layers-svgrepo-com.svg">
+    </div>
 </template>
   
 <script>
     import { onMounted } from "vue";
     import { initCesium } from "../scripts/displayMap";
+    import './cesium.css'
   
     export default {
         setup() {
@@ -19,6 +23,27 @@
     #cesiumContainer{
         width: 100vw;
         height: 100vh;
+    }
+    #layerPicker{
+        position: absolute;
+        right: 5px;
+        top: 5px;
+        width: 32px;
+        height: 32px;
+        border-radius: 14%;
+        background-color: #303336;
+        border: 1px solid #444;
+    }
+    #layerPicker img{
+        width: 28px;
+        height: 28px;
+        margin-left: 2px;
+        margin-top: 2px;
+    }
+    #layerPicker:hover{
+        background-color: #4488bb;
+        box-shadow: 0px 0px 50px #4488bb;
+        border: 1px solid #aaeeff;
     }
 
 </style>
